f0bee6297a5482d03560657850564ab746c3e3bc
Leo Durrant <poweredbykurisu@hotmail.com>
2024-04-07 15:06:18 +0100

    fix: logic for preventing running project to be added

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ import sys
 resp = requests.get(f"https://api.github.com/orgs/{sys.argv[1]}/repos")
 output = []
 
+print(f"Polling repositories for {sys.argv[1]}")
+
 for i in resp.json():
-    if (i["homepage"] is not None) or i['name'] == sys.argv[2]:
+    if (i["homepage"] is not None) or i['name'] != sys.argv[2]:
         output.append(f"\t\t<tr><td><a href=\"{i["homepage"]}\">{i["name"]}</a></td><td><a href=\"{i["html_url"]}\">Source</a></td></tr>")
+
+print(f"Created {len(output)} link(s) for discovered projects.")
 
 with open('template.html', 'r') as f:
     templateText = Template(f.read())
@@ -19,3 +23,4 @@ with open('output/index.html', 'w') as fw:
     soup = BeautifulSoup(templateText.substitute(body='\n'.join(output), footer=f'<i>Generated on {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} for {sys.argv[1]}</i>'), "html.parser")
     fw.writelines(soup.prettify())
     
+print("Done!")
